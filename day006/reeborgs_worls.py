8f099842559949c240e8344f57c90b900a186660
Jithendra1101 <gjithendravarma@gmail.com>
2023-12-11 21:57:53 +0530

    added day006

---
+++ day006/reeborgs_worls.py
@@ -0,0 +1,28 @@
+''' hurdle 1,2,3,4
+
+https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
+'''
+
+''' 
+def mov_right():
+    for i in range(3):
+        turn_left()
+def passhurdle():
+    turn_left()
+    while wall_on_right():
+        move()
+    mov_right()
+    move()
+    mov_right()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+   if wall_in_front():
+    passhurdle()
+   else :
+    move()
+
+
+
+'''
